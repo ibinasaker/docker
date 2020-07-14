578f953e4752d444b367854bd56cf612bf23673e
Ibrahim Bin Asaker <eng.binasaker@gmail.com>
2020-07-15 02:08:06 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -5,7 +5,7 @@ sudo apt update -y
 sudo apt install -y gnupg1
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+sudo add-apt-repository -y  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt update -y
