117b217076c957856dacdcec2413e2a19a8c4fa7
Ibrahim Bin Asaker <eng.binasaker@gmail.com>
2020-07-15 08:46:36 +0300

    Update install_docker.sh
    add comments

--- install_docker.sh
+++ install_docker.sh
@@ -1,16 +1,32 @@
-# script to automate docker install on Ubuntu/Debian 
+## this script to automate docker install on Ubuntu/Debian linux distributions
 sudo apt update -y
+
+# install pre-required packages
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+# reinstal gpg in case the if the packages is broken (sometimes ubuntu shipped with broken gpg)
 sudo apt remove gpg
 sudo apt update -y
 sudo apt install -y gnupg1
+
+# add docker key for the approperiate distribution  
 dist_name=$(cat /etc/issue | grep -Eo '^[^ ]+' | tr '[:upper:]' '[:lower:]')
 curl -fsSL https://download.docker.com/linux/$dist_name/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
+
+# install docker-ce for the approperiate distribution
 sudo add-apt-repository -y  "deb [arch=amd64] https://download.docker.com/linux/$dist_name \
    $(lsb_release -cs) \
    stable"
 sudo apt update -y
 sudo apt install -y docker-ce 
+
+# add user to docker group to avoid needing for sudo when typing docker commands
 sudo groupadd docker
 sudo usermod -a -G docker $USER
+
+# Install Python 3 and PIP.
+sudo apt-get install -y python3 python3-pip
+
+# Install Docker Compose into your user's home directory.
+pip3 install --user docker-compose
