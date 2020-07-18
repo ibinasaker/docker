674c7d79f9deef3244dfa60b1e8a14c7bf46a52d
Ibrahim Bin Asaker <eng.binasaker@gmail.com>
2020-07-18 08:54:05 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,28 +1,25 @@
 ## this script to automate docker install on Ubuntu/Debian linux distributions
 sudo apt update -y && sudo apt autoremove -y && sudo apt autoclean -y
 
-# install prerequest packages
-sudo apt install -y apt-transport-https
-sudo apt install -y ca-certificates
-sudo apt install -y curl
-sudo apt install -y software-properties-common
+# install pre-required packages
+sudo apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    gnupg-agent \
+    curl \
+    software-properties-common
 
-# reinstall gpg in case the if the packages is broken (sometimes ubuntu shipped with broken gpg)
-sudo apt remove -y gpg
-sudo apt update -y
-sudo apt install -y gnupg1
-
-# add docker key for the approperiate distribution  
+# add docker key for the approperiate distribution
 dist_name=$(cat /etc/issue | grep -Eo '^[^ ]+' | tr '[:upper:]' '[:lower:]')
 curl -fsSL https://download.docker.com/linux/$dist_name/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 
 # install docker-ce for the approperiate distribution
-sudo add-apt-repository -y  "deb [arch=amd64] https://download.docker.com/linux/$dist_name \
-   $(lsb_release -cs) \
-   stable"
+dist_name=$(cat /etc/issue | grep -Eo '^[^ ]+' | tr '[:upper:]' '[:lower:]')
+sudo add-apt-repository -y  \
+    "deb [arch=amd64] https://download.docker.com/linux/$dist_name $(lsb_release -cs) stable"
 sudo apt update -y
-sudo apt install -y docker-ce 
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # add user to docker group to avoid needing for sudo when typing docker commands
 sudo groupadd docker
@@ -34,5 +31,6 @@ sudo apt-get install -y python3 python3-pip
 # Install Docker Compose into your user's home directory.
 pip3 install --user docker-compose
 
-RED='\033[0;31m'
-printf "${RED}Please logout and re-login in order to run Docker!\n"
+LBLUE='\033[1;34m'
+NC='\033[0;35m'
+printf "${LBLUE}To test run ${NC}docker run hellow-world ${LBLUE}if you faces error try relogin!\n"
