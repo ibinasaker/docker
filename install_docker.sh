d16d019569fc7452b8a871901f8e7bb36a1741c5
Ibrahim Bin Asaker <eng.binasaker@gmail.com>
2020-07-18 10:05:01 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -39,4 +39,5 @@ sudo service docker start
 LBLUE='\033[1;34m'
 NC='\033[0;35m'
 printf "${LBLUE}To test run ${NC}docker run hellow-world\n"
-printf "${LBLUE}if you have docker deamon issue try ${NC}sudo service docker start ${LBLUE} if you face permission issues try relogin!\n"
+printf "${LBLUE}if you have docker deamon issue try running ${NC}sudo service docker start \n"
+printf "${LBLUE}if you face permission issues try relogin!\n"
